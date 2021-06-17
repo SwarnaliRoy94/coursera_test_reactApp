f7c09669aeeb0a87b3be4d5111c26d020e59fb97
Swarnali Roy <swarnali.roy093@gmail.com>
2021-06-18 01:45:32 +0600

    react redux form assignment done

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Control, Errors, LocalForm } from "react-redux-form";
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
   Button,
   Modal,
   ModalHeader,
@@ -39,6 +31,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     alert("Current State is: " + JSON.stringify(values));
+    // event.preventDefault();
   }
 
   render() {
@@ -121,73 +114,4 @@ class CommentForm extends Component {
   }
 }
 
-function RenderDish({ dish }) {
-  if (dish != null)
-    return (
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    );
-  else return <div></div>;
-}
-
-function RenderComments({ comments }) {
-  if (comments !== null) {
-    return (
-      <ul className="list-unstyled">
-        {comments.map((comment) => {
-          return (
-            <li>
-              <div>{comment.comment}</div>
-              <div>
-                <p>
-                  -- {comment.author}{" "}
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  }).format(new Date(Date.parse(comment.date)))}
-                </p>
-              </div>
-            </li>
-          );
-        })}
-        <CommentForm />
-      </ul>
-    );
-  } else return <div></div>;
-}
-
-const DishDetail = (props) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish dish={props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <h4>Comments</h4>
-          <RenderComments comments={props.comments} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default DishDetail;
+export default CommentForm;
