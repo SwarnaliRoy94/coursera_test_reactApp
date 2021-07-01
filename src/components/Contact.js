8fcff8e8dcf58396d58cdc21b015d4bd4e26bb81
Swarnali Roy <swarnali.roy093@gmail.com>
2021-07-01 19:41:05 +0600

    react redux form revisited

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Control, Errors, LocalForm } from "react-redux-form";
+import { Control, Errors, Form, LocalForm } from "react-redux-form";
 import { Link } from "react-router-dom";
 import {
   Breadcrumb,
@@ -27,6 +27,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
     // event.preventDefault();
   }
 
@@ -97,7 +98,10 @@ class Contact extends Component {
             <h3>Send us your feedback!</h3>
           </div>
           <div className="col-12 col-md-9">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form
+              model="feedback"
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
               <Row className="form-group">
                 <Label htmlFor="firstname" md={2}>
                   First Name
@@ -259,7 +263,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
